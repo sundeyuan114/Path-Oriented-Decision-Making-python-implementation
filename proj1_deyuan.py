254e2f9e082ee08c67375f3ab58464ad314629a1
Deyuan Sun <sundeyuan114@gmail.com>
2023-09-04 18:42:20 -0400

    finished building gates and nets, will do simulation tmr

--- proj1_deyuan.py
+++ proj1_deyuan.py
@@ -14,7 +14,7 @@ class Gate():
         self.gate3 = gate3
     def print_self(self):
         print(self.name, self.value, self.number, self.gate1, self.gate2, self.gate3)
-        pass
+        
 class Net():
     def __init__(self, number = -1, input_gate = -1, output_gate_list = []):
         self.number = number
@@ -31,22 +31,58 @@ def Read_netlist(input_file_name:str):
         lineno = 0
         for line in infile:
             temp = line.strip().split()
-            gate_node = Gate(name= temp[0], number= lineno, gate1= temp[1], gate2 = temp[2], gate3 = temp[3] if len(temp) > 3 else -1)
-            #gate part works fine so far
-            for net_num in temp[1:]:
-                if net_num not in net_list:
-                    net_list[net_num] = Net(number= net_num, input_gate= -1 if net_num < (len(temp)-1) else lineno, output_gate_list=[])
-
-            gate_list.append(gate_node)
+            if (temp[0] != "INPUT" and temp[0] != "OUTPUT"):
+                gate_node = Gate(name= temp[0], number= lineno, gate1= temp[1], gate2 = temp[2], gate3 = temp[3] if len(temp) > 3 else -1)
+                #
+                # gate part works fine so far
+                #
+                for net_num_index in range(1, len(temp)):
+                    # if first time seen in dict
+                    net_num = temp[net_num_index]
+                    if net_num not in net_list:
+                        if int(net_num_index) < (len(temp)-1):
+                            net_list[net_num] = Net(number= net_num, input_gate= -1
+                                                , output_gate_list= [lineno])
+                        else:
+                            net_list[net_num] = Net(number= net_num, input_gate= lineno
+                                                , output_gate_list= [])
+                        
+                    else:
+                    #extend the dict values
+                        if int(net_num_index) == (len(temp)-1):
+                            net_list[net_num].input_gate = lineno
+                        else:
+                            net_list[net_num].output_gate_list.append(lineno)
+                        pass
+                    if net_num == 4:
+                        print(net_num, )
+                #
+                #
+                #
+                print(net_num, (len(temp)-1), lineno)
+            else:
+                pass
             lineno += 1
+            
     pass
     # read the netlist file and build up 2 data structures for gates and nets
-    print (gate_list[0].print_self())
-    
-    #print (gate_list[1].print_self())
+    print (net_list.keys())
+    for value in net_list.keys():
+        print (net_list[value].print_self())
+    # print (net_list['1'].number, net_list['1'].input_gate, net_list['1'].output_gate_list)
 
 
 
+
+    # print(self.name, self.value, self.number, self.gate1, self.gate2, self.gate3)
+    # for gate in gate_list:
+    #     gate.print_self()
+    # AND -1 0 1 2 4
+    # OR -1 1 2 3 5
+    # INV -1 2 4 6 -1
+    # XOR -1 3 4 5 7
+
+    #gate looks fine
 
 def Simulation():
     pass
