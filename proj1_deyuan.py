aedc497ea8521992d428327d5d0256f7094956ae
Deyuan Sun <sundeyuan114@gmail.com>
2023-09-05 18:45:29 -0400

    data structure build over

--- proj1_deyuan.py
+++ proj1_deyuan.py
@@ -1,19 +1,27 @@
 #Deyuan Sun ece6140 proj1 code starting on 9/4/2023
 import sys
-from enum import Enum
 
+def test_print(gate_list:[], net_list:{}):
+    print (net_list.keys())
+    for value in net_list.keys():
+        net_list[value].print_self()
+    print("----------------")
+    for gate in gate_list:
+        gate.print_self()
+    print("----------------")
+    
 
 # Limit the input str, making sure weird gates does not exist
 class Gate():
-    def __init__(self, name = -1, value = -1, number = -1, gate1 = -1, gate2 = -1, gate3 = -1):
+    def __init__(self, name = -1, value = -1, number = -1, net1 = -1, net2 = -1, net3 = -1):
         self.name = name
         self.value = value
         self.number = number
-        self.gate1 = gate1
-        self.gate2 = gate2
-        self.gate3 = gate3
+        self.net1 = net1
+        self.net2 = net2
+        self.net3 = net3
     def print_self(self):
-        print(self.name, self.value, self.number, self.gate1, self.gate2, self.gate3)
+        print(self.name, self.value, self.number, self.net1, self.net2, self.net3)
         
 class Net():
     def __init__(self, number = -1, input_gate = -1, output_gate_list = []):
@@ -22,19 +30,30 @@ class Net():
         self.output_gate_list = output_gate_list
     def print_self(self):
         print(self.number, self.input_gate, self.output_gate_list)    
-
+        
 gate_list = []
 net_list = {}
+# input_gate_list = []
+# output_gate_list = []
 
-def Read_netlist(input_file_name:str):
+def Read_netlist(input_file_name:str) -> []:
     with open("./files/" + input_file_name, "r") as infile:
         lineno = 0
+        
         for line in infile:
             temp = line.strip().split()
             if (temp[0] != "INPUT" and temp[0] != "OUTPUT"):
-                gate_node = Gate(name= temp[0], number= lineno, gate1= temp[1], gate2 = temp[2], gate3 = temp[3] if len(temp) > 3 else -1)
                 #
-                # gate part works fine so far
+                # gate part 
+                #
+                gate_node = Gate(name= temp[0], number= lineno, net1= temp[1], net2 = temp[2], net3 = temp[3] if len(temp) > 3 else -1)
+                gate_list.append(gate_node)
+                #
+                # gate part 
+                #
+
+                #
+                # net part below
                 #
                 for net_num_index in range(1, len(temp)):
                     # if first time seen in dict
@@ -54,27 +73,24 @@ def Read_netlist(input_file_name:str):
                         else:
                             net_list[net_num].output_gate_list.append(lineno)
                         pass
-                    if net_num == 4:
-                        print(net_num, )
                 #
+                # net part above
                 #
-                #
-                print(net_num, (len(temp)-1), lineno)
             else:
-                pass
+                if temp[0] == "INPUT":
+                    ilist = [temp[x] for x in range(1,len(temp))]
+                else:
+                    olist = [temp[x] for x in range(1,len(temp))]
+                    return [ilist, olist]
             lineno += 1
-            
-    pass
     # read the netlist file and build up 2 data structures for gates and nets
-    print (net_list.keys())
-    for value in net_list.keys():
-        print (net_list[value].print_self())
+    # test_print(gate_list = gate_list, net_list = net_list)
     # print (net_list['1'].number, net_list['1'].input_gate, net_list['1'].output_gate_list)
 
 
 
 
-    # print(self.name, self.value, self.number, self.gate1, self.gate2, self.gate3)
+    # print(self.name, self.value, self.number, self.net1, self.net2, self.net3)
     # for gate in gate_list:
     #     gate.print_self()
     # AND -1 0 1 2 4
@@ -98,4 +114,6 @@ if __name__ == "__main__":
     pass
     # assign input file and output file here.
     input_file_name = sys.argv[1]
-    Read_netlist(input_file_name)
+    input_output_lists = Read_netlist(input_file_name)
+    test_print(gate_list = gate_list, net_list = net_list)
+    print(input_output_lists)
